773963b217ad212d2ad377cb6cf7f23e4c958dfc
DDemmer1 <Unknown@DESKTOP-4KSK0J5>
2021-07-13 10:54:55 +0200

    add bounds check

--- src/de/demmer/dennis/snake/entity/Head.java
+++ src/de/demmer/dennis/snake/entity/Head.java
@@ -2,12 +2,14 @@ package de.demmer.dennis.snake.entity;
 
 import java.awt.Color;
 
+import de.demmer.dennis.snake.configuration.SnakeConfig;
+
 public class Head extends Block {
 
 	public enum HeadDirection {
 		UP, DOWN, LEFT, RIGHT
 	}
-	
+
 	public static HeadDirection direction;
 
 	private int lastX;
@@ -33,6 +35,32 @@ public class Head extends Block {
 	public void setLastY(int lastY) {
 		this.lastY = lastY;
 	}
+
+	@Override
+	public void setBlockY(int blockY) {
+
+		if (blockY < 0) {
+			super.setBlockY(SnakeConfig.BOARD_SIZE - 1);
+		} else if ( blockY > SnakeConfig.BOARD_SIZE - 1) {
+			super.setBlockY(0);
+		} else {
+			super.setBlockY(blockY);
+		}
+	}
+	
+	@Override
+	public void setBlockX(int blockX) {
+
+		if (blockX < 0) {
+			super.setBlockX(SnakeConfig.BOARD_SIZE - 1);
+		} else if ( blockX > SnakeConfig.BOARD_SIZE - 1) {
+			super.setBlockX(0);
+		} else {
+			super.setBlockX(blockX);
+		}
+	}
+	
+	
 
 	public void move() {
 		setLastX(getBlockX());
